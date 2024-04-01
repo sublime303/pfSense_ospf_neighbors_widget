b3fdf0ea657d32d88bb171b19e7d5eeb8396d20c
Benjamin Kraal <benjamin.kraal@bahnhof.net>
2024-04-01 14:27:13 +0200

    update

--- widgets/ospf.widget.php
+++ widgets/ospf.widget.php
@@ -1,18 +1,26 @@
 <?php
 
+
+/*
+|--------------------------------------------------------------------------
+| OSPF Neighbors widget
+|--------------------------------------------------------------------------
+|
+| A widget to display OSPF neighbors, parsing the output of the "show ip ospf neighbor" command.
+| Requires the FRR routing package to be installed and OSPF to be configured.
+| Then add the widget to the dashboard from usual widget menu.
+|
+*/
+
 $lines = str_getcsv($output, "\n");
+
 foreach ($lines as $num => $line) {
-   
-    $keywords = preg_split("/[\s,]+/", $line);
-    if ($num > 1){
-        $all[] = $keywords;
-    }
-    //     $all[] = $keywords;
-    //     $titles[] = $keywords;
-    // #print_r($keywords);
-    // unset($all[0]);
-    $titles = ['NeighborID','Pri','State','UpTime','DeadTime','Address','Interface']; #,'RXmtL','RqstL','DBsmL'];
+    $words = preg_split("/[\s,]+/", $line);
+    if ($num > 1)
+        $all[] = $words;
 }
+
+$titles = ['NeighborID','Pri','State','UpTime','DeadTime','Address','Interface']; #,'RXmtL','RqstL','DBsmL'];
 ?>
 
 <div class="table-responsive">
@@ -21,32 +29,26 @@ foreach ($lines as $num => $line) {
             <tr>
                 
                 <?php
-                foreach ($titles as $word) {
-                    echo "<th>$word</th>";
+                foreach ($titles as $title) {
+                    echo "<th>$title</th>";
                 }
                 ?>
             </tr>
         </thead>
+        
         <tbody>
             
-        <?php
-        foreach ($all as $num => $word) {
-            echo "<tr>";
-                foreach ($word as $num => $w) {
-                    if ($num < 7) 
-                    echo "<td>$w</td>";
-                }
-            echo "</tr>";
-        }
-        ?>
+            <?php
+            foreach ($all as $num => $word) {
+                echo "<tr>";
+                    foreach ($word as $num => $w) {
+                        if ($num < 7) 
+                        echo "<td>$w</td>";
+                    }
+                echo "</tr>";
+            }
+            ?>
         
         </tbody>
     </table>
 </div>
-
-
-
-
-
-
-
